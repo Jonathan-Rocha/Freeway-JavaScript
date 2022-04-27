b652238b347351fa7c4b010f9e25b4afcbb6e8f9
Jonathan Rocha <johnrocha149@gmail.com>
2022-04-27 11:03:19 -0300

    commit-inicial

---
+++ Freeway/sketch.js
@@ -0,0 +1,30 @@
+
+
+
+function setup() {
+  createCanvas(500, 400);
+  somTrilha.loop();
+
+}
+
+function draw() {
+  background(imagemEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
+
+
+
+
+
+
+
+
+
